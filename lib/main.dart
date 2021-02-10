c256396b5bccf52cf50ac1b435e45c31475fdd1d
emtalen <59577366+emtalen@users.noreply.github.com>
2021-02-10 16:56:58 +0100

    adding a screen for adding a new place

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 import 'package:provider/provider.dart';
 import './providers/places.dart';
 
@@ -13,14 +14,13 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: Places(),
       child: MaterialApp(
-        title: 'Great Places',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.blueAccent
-        ),
-        home: PlacesListScreen(),
-      ),
+          title: 'Great Places',
+          theme: ThemeData(
+              primarySwatch: Colors.indigo, accentColor: Colors.blueAccent),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
     );
   }
 }
-
